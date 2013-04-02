05ad4a335c4464fde7f2f5ff5bc2ea6a6aa7e261
Miles Whiticker <miles.whiticker@gmail.com>
2013-04-03 00:13:00 +1000

    added a bit of extra info and a new define (ALLDIRS) to Direction.h
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- Space_Fortress_12/Direction.h
+++ Space_Fortress_12/Direction.h
@@ -1,11 +1,21 @@
 #ifndef DIRECTION_H
 #define DIRECTION_H
 
+/*
+NORTH 1		//+Z	UNIT_Z
+SOUTH 2		//-Z	NEGATIVE_UNIT_Z
+EAST 4		//+X	UNIT_X
+WEST 8		//-X	NEGATIVE_UNIT_X
+UP 16		//+Y	UNIT_Y
+DOWN 32		//-Y	NEGATIVE_UNIT_Y
+*/
+
 #define NORTH 1		//+Z
 #define SOUTH 2		//-Z
 #define EAST 4		//+X
 #define WEST 8		//-X
 #define UP 16		//+Y
 #define DOWN 32		//-Y
+#define ALLDIRS (NORTH|SOUTH|EAST|WEST|UP|DOWN)
 
 #endif	DIRECTION_H
